9a5b14cfd1d279c8115ef7f63c9a0e6b0fffefa0
esteemedtogami <sam.pickell01@gmail.com>
2019-03-23 18:35:02 -0400

    Also updated the date in the headers

--- JS/pathfinder.js
+++ JS/pathfinder.js
@@ -1,7 +1,7 @@
 /*
   Copyright (C) 2019 Jeff Quattrociocchi, Sam Pickell, Stanley Chan, Peter Louisne
   UMass Lowell CS Students, COMP 4620 GUI II
-  March 21, 2019
+  March 23, 2019
   GUI II Pathfinder Group 6, javascript
 */
 //temporary global variables
