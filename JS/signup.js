f63d2c9b2e3aa053baffb003727800124cda2e5a
esteemedtogami <sam.pickell01@gmail.com>
2019-04-11 16:43:13 -0400

    Databasing and sign up page

---
+++ JS/signup.js
@@ -0,0 +1,6 @@
+/*
+  Copyright (C) 2019 Jeff Quattrociocchi, Sam Pickell, Stanley Chan, Peter Louisne
+  UMass Lowell CS Students, COMP 4620 GUI II
+  March 23, 2019
+  GUI II Pathfinder Group 6, javascript for index page
+*/
